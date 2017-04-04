d6527310b249566e86035820044f0a7b911ee5bc
Eduardo Pinuaga Linares <info@did-web.com>
2017-04-04 16:57:51 +0200

    Progress with Contao

---
+++ config/config.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Back end modules
+ */
+$GLOBALS['BE_MOD']['content']['querySIS'] = array(
+	'tables' => array('tl_querySIS'),
+	'icon'   => 'system/modules/querySIS/assets/images/querySIS.png'
+);
---
+++ languages/modules.php
@@ -0,0 +1,5 @@
+<?php
+ 
+$GLOBALS['TL_LANG']['MOD']['querySIS'][0] = 'querySIS';
+$GLOBALS['TL_LANG']['MOD']['screencast'][1] = 'querySIS';
+
