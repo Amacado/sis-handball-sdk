a4616ff4a26c8aa9e9dad263bf7a18a12654e54d
Eduardo Pinuaga Linares <info@did-web.com>
2017-04-06 09:13:04 +0200

    First configuration for QuerySIS module Contao. Only menu settings in Backend.

--- config/config.php
+++ config/config.php
@@ -1,8 +1,35 @@
 <?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2016 Leo Feyer
+ *
+ * @license LGPL-3.0+
+ */
+
+
+/**
+ * Add content element
+ */
+$GLOBALS['TL_CTE']['includes']['querySIS'] = 'ContentQuerySIS';
+
+
+/**
+ * Front end modules
+ */
+$GLOBALS['FE_MOD']['application']['querySIS'] = array('querySIS_list' => 'ModuleQuerySISList');
+
+
 /**
  * Back end modules
  */
-$GLOBALS['BE_MOD']['content']['querySIS'] = array(
-	'tables' => array('tl_querySIS'),
-	'icon'   => 'system/modules/querySIS/assets/images/querySIS.png'
-);
+array_insert($GLOBALS['BE_MOD']['ElementSystem'], 0, array
+(
+	'querySIS' => array
+	(
+		'file'     => array('querySISmodul'),
+		'icon'       => 'system/modules/querySIS/assets/images/icon.png',
+		'stylesheet' => 'system/modules/querySIS/assets/css/style.css'
+	)
+));
--- languages/modules.php
+++
@@ -1,5 +0,0 @@
-<?php
- 
-$GLOBALS['TL_LANG']['MOD']['querySIS'][0] = 'querySIS';
-$GLOBALS['TL_LANG']['MOD']['screencast'][1] = 'querySIS';
-
