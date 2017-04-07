b282cf38bafe19b26e4ccfb33a503ca81ada4be6
Eduard Pinuaga <info@did-web.com>
2017-04-07 10:41:54 +0200

    Clean Files

--- config/config.php
+++ config/config.php
@@ -5,15 +5,15 @@
 $GLOBALS['BE_MOD']['querySIS'] = array(
     
     'Query' => array(
-	'tables' => array('tl_qSIS_query'), //'tables'      => array('tl_article', 'tl_content'),
+	'tables' => array('tl_qSIS_query'), 
 	'icon'   => 'system/modules/querySIS/assets/images/icon.png'
         ),
      'Liga' => array(
-	'tables' => array('tl_qSIS_liga'), //'tables'      => array('tl_article', 'tl_content'),
+	'tables' => array('tl_qSIS_liga'),
 	'icon'   => 'system/modules/querySIS/assets/images/icon.png'
         ),
     'Verein' => array(
-	'tables' => array('tl_qSIS_verein'), //'tables'      => array('tl_article', 'tl_content'),
+	'tables' => array('tl_qSIS_verein'),
 	'icon'   => 'system/modules/querySIS/assets/images/icon.png'
         ),
     
@@ -26,6 +26,6 @@ $GLOBALS['BE_MOD']['querySIS'] = array(
 $GLOBALS['FE_MOD']['querysis'] = array
 (
 	
-        'qSISquery_list'     => 'ModuleQuerySISListQuery',
-        'qSIS_verein_list'     => 'ModuleQuerySISListVerein',
+       // 'qSISquery_list'     => 'ModuleQuerySISListQuery',
+       // 'qSIS_verein_list'     => 'ModuleQuerySISListVerein',
 );
