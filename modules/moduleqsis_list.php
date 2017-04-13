1264965d3fb455f39102f3b878416f14f5f2785c
Eduard Pinuaga <info@did-web.com>
2017-04-13 14:02:44 +0200

    Basic function paginator ist work. Uncleaned code

--- modules/moduleqsis_list.php
+++ modules/moduleqsis_list.php
@@ -1,6 +1,6 @@
 <?php
-//require_once  ('system/modules/querysis/classes/querySis.php');  
-require_once  ('c:/xampp/htdocs/contaotest/system/modules/querysis/classes/querySis.php');  
+//require_once  ('system/modules/querysis/classes/querySis.php');
+require_once  ('c:/xampp/htdocs/contaotest/system/modules/querysis/classes/querySis.php');
 //namespace Contao;
 
 class moduleqsis_list extends \ContentElement //\Module
@@ -10,76 +10,93 @@ class moduleqsis_list extends \ContentElement //\Module
 	 * @var string
 	 */
         protected $strTemplate ='';
- 
+
         // n registrer for page.
-        private $regPage = 2;
-        
+        private $regPage = 4;
+
 	/**
 	 * Compile the current element
 	 */
 	protected function compile()
 	{
-      
+
             /** @var \PageModel $objPage */
             global $objPage;
-        
-        
-        
-       
+
+
+
+
             if($this->querysis){
             $myclass =  new querySis();
-            
+
                 $rs = Database::getInstance()
                     ->prepare('SELECT tl_qsis_verein.name, tl_qsis_verein.code, tl_qsis_verein.image, tl_qsis_liga.code as codeLiga FROM '
                             . 'tl_qsis_verein INNER JOIN  tl_qsis_liga ON tl_qsis_liga.id = tl_qsis_verein.idLiga'
                             . ' WHERE tl_qsis_verein.id= ? ')
                     ->execute($this->querysisTeam);
                 $agent = $rs->fetchAllAssoc();
-  
 
 
 
-//            $objFiles = FilesModel::findMultipleByIds(deserialize($agent[0]['image']));  
-//            $strImage = $this->getImage($objFiles->path, 235, 130, 'center_center');  
-//             echo "/".$strImage; 
+
+//            $objFiles = FilesModel::findMultipleByIds(deserialize($agent[0]['image']));
+//            $strImage = $this->getImage($objFiles->path, 235, 130, 'center_center');
+//             echo "/".$strImage;
                 $valorUrlImageRival = "";
                 switch ($this->querysis) {
                         case "Tabelle":
-                          
-                            $this->strTemplate='mod_qsis_list_tabelle'; 
-                        
+
+                            $this->strTemplate='mod_qsis_list_tabelle';
+
                             $listQuery = $myclass->tabelleGames($agent[0]['codeLiga']);
                             break;
+
+
                         case "Next":
                            $this->strTemplate='mod_qsis_list_next';
                            $listQuery = $myclass->nextGames($agent[0]['code'], 1);
                             break;
+
+
                         case "All":
                             $this->strTemplate='mod_qsis_list_all';
+
                             $listQuery = $myclass->allGames($agent[0]['codeLiga']);
+
                             $totalPage = count($listQuery)/ $this->regPage;
-                            
+
                             if($this->Input->get('page')){
                                 $pageGet = $this->Input->get('page');
                             } else {
                                 $pageGet = 1;
                             }
-                            $listQuery = array_slice($listQuery, 0, $this->regPage);
+
+														if($pageGet==1){
+															$initCout = 0;
+														} else{
+
+															$initCout = $pageGet*$this->regPage;
+														}
+
+
+                            $listQuery = array_slice($listQuery, $initCout, $this->regPage);
                             break;
+
+
                         case "NextHaus":
                             $this->strTemplate='mod_qsis_list_nexthaus';
-                            
-                            
-                            
+
+
+
                             $listQuery = $myclass->nextGames($agent[0]['codeLiga'], 30);
-                          
-                            
+
+
                             $nextInHaus = array();
                             $n=0;
                             foreach($listQuery as $value){
-                                
+
                                 if($value->getHeimNr()==$agent[0]['code']){
-                                    
+
                                     $nextInHaus[$n]['Gast'] = $value->getGast();
                                     $nextInHaus[$n]['GastNr'] = $value->getGastNr();
                                     $nextInHaus[$n]['SpielDatum'] = $value->getSpielDatum();
@@ -88,42 +105,42 @@ class moduleqsis_list extends \ContentElement //\Module
                                     $nextInHaus[$n]['HallenOrt'] = $value->getHallenOrt();
                                     $n++;
                                 }
-                                
+
                             }
-                            
+
                              $rsImg = Database::getInstance()
                                         ->prepare('SELECT tl_qsis_verein.id, tl_qsis_verein.code, tl_qsis_verein.image FROM '
                                                 . 'tl_qsis_verein '
                                                 . ' WHERE tl_qsis_verein.code=  '.$nextInHaus[0]['GastNr'])
                                         ->execute();
-                             
+
                             $imgGast = $rsImg->fetchAllAssoc();
-                            
-                            $objFiles = FilesModel::findMultipleByIds(deserialize($imgGast[0]['image']));  
-                            $strImage = $this->getImage($objFiles->path, 100, 100, 'center_center');  
-                           
-                            $valorUrlImageRival =   "./".$strImage; 
-                
-                
+
+                            $objFiles = FilesModel::findMultipleByIds(deserialize($imgGast[0]['image']));
+                            $strImage = $this->getImage($objFiles->path, 100, 100, 'center_center');
+
+                            $valorUrlImageRival =   "./".$strImage;
+
+
                             $nextInHaus[0]['GastImage'] = $valorUrlImageRival;
                             $listQuery = $nextInHaus;
                             break;
                 }
             }
-        $this->Template = new FrontendTemplate($this->strTemplate);  
- 
+        $this->Template = new FrontendTemplate($this->strTemplate);
+
         if($this->querysis=="All"){
             // Data for pagination.
             $this->Template->totalPage =  $totalPage;
-            $this->Template->pageActuale = $pageGet;
+            $this->Template->pageGet = $pageGet;
             }
 
-        
+
         $this->Template->urlImageRival =  $valorUrlImageRival;
-        $this->Template->resultsQuery =  $listQuery; 
+        $this->Template->resultsQuery =  $listQuery;
         $this->Template->nOurVerein = $agent[0]['code'];
         $this->Template->typeQuery = $this->querysis;
-              
+
         }
-	
+
 }
